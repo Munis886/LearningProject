ee5c27d6f1c3408979a4d7a7f49ae1ad26e7e40f
Munis886 <bmunis886@gmail.com>
2026-02-09 16:25:37 +0500

    yangi

---
+++ 1.Front end/3.Js/3.Shartli ifodalar/3.1.Shartli ifodalarga kirish/main.js
@@ -0,0 +1,9 @@
+let yosh = prompt("Yoshingizni kiriting: ");
+
+if (yosh >= 20) {
+    console.log("Siz kirishingiz mumkin, 20 yoshdan kattasiz!");
+} else if (yosh >= 7) {
+    console.log("Siz maktabga boring");
+} else {
+    console.log("Siz yoshingiz 7 yoshdan kichik, boqchaga boring");
+}
