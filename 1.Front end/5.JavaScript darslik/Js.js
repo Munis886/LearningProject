a98d601f201eb7233401f4aaa4956a6b8e9d82c0
Munis886 <bmunis886@gmail.com>
2026-04-13 11:55:48 +0500

    yangi

---
+++ 1.Front end/5.JavaScript darslik/Js.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  var content = document.getElementById("container");
+  menu.classList.toggle("active");
+  content.classList.toggle("shifted");
+}
+
+function toggleSubMenu(id) {
+  var subMenu = document.getElementById(id);
+  if (subMenu.style.display === "block") {
+    subMenu.style.display = "none"; // Yashirish
+  } else {
+    subMenu.style.display = "block"; // Ko'rsatish
+  }
+}
+
+
